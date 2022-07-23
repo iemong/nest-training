061db55f1a26096e67d9645e8367a6f969a1b650
iemong <kiyoshidainagon@gmail.com>
2022-07-24 00:16:04 +0900

    add post module

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postService: PostsService) {}
+
+  @Get()
+  getData() {
+    return this.postService.get();
+  }
+
+  @Post()
+  postData(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.postService.store(title, description);
+  }
+
+  // :idと表記することで、urlのidを取得できる
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.postService.update(id, title, description);
+  }
+}
