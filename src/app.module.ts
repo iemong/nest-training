c96c4861ac4f05f7871d68f52e04530bb7b312eb
iemong <kiyoshidainagon@gmail.com>
2022-07-24 23:36:15 +0900

    sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Posts } from './posts/posts.entity';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sqlitedb.db',
+      entities: [Posts],
+      synchronize: true,
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Posts } from './posts.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Posts])],
   controllers: [PostsController],
   providers: [PostsService],
 })
